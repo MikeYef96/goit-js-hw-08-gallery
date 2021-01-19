9cbde8550717d8308f160b344e19f78b4bcb6dc9
MikeYef96 <mike.yef@gmail.com>
2021-01-19 19:06:17 +0300

    updete repository

--- js/gallery-workpage.js
+++
@@ -1,9 +0,0 @@
-import gallery from "./gallery.js";
-
-const ref = {
-  galleryList: document.querySelector("gallery__list"),
-  lightbox: document.querySelector("lightbox"),
-  lightboxOverlay: document.querySelector("lightbox__overlay"),
-};
-
-gallery.forEach(() => ref.galleryList.createElement("li"));
