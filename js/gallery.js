7577ee2b63687fbe904413d664c702cb7535f7a7
MikeYef96 <mike.yef@gmail.com>
2021-01-21 21:10:39 +0300

    task for mark === 10

--- js/gallery.js
+++ js/gallery.js
@@ -1,81 +1,89 @@
 import galleryItems from "./gallery-items.js";
 
 const refs = {
-  gallery: document.querySelector(".js-gallery"),
-  modalWindow: document.querySelector(".lightbox"),
-  content: document.querySelector(".lightbox__content"),
-  image: document.querySelector(".lightbox__image"),
-  btnCloseLightbox: document.querySelector(
-    "button[data-action='close-lightbox']"
-  ),
+  gallery: document.querySelector(".gallery"),
+  modalOpen: document.querySelector(".lightbox"),
+  modalContent: document.querySelector(".lightbox__content"),
+  modalImage: document.querySelector(".lightbox__image"),
+  modalCloseBtn: document.querySelector('button[data-action="close-lightbox"]'),
 };
+
+let galleryListItems = document.createDocumentFragment();
 
 galleryItems.forEach((item) => {
-  let imgItem = `<li class='gallery__item'>
-    <a class='gallery__link' href=${item.original}>
-     <img class="gallery__image"
-     src=${item.preview} 
-     data-source=${item.original} 
-     alt=${item.description}/>
-   </a>
- </li>`;
-  refs.gallery.insertAdjacentHTML("beforeend", imgItem);
+  galleryListItems = `<li class="gallery__item">
+  <a
+      class="gallery__link"
+      href=${item.original}
+    >
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</li>`;
+  refs.gallery.insertAdjacentHTML("beforeend", galleryListItems);
 });
 
-let originItemsArr = [];
-galleryItems.forEach((item) => originItemsArr.push(item.original));
+let originalItems = [];
+galleryItems.forEach((item) => {
+  originalItems.push(item.original);
+});
 
-const closeOverlayContent = (event) => {
-  if (event.target !== event.currentTarget) return;
-  closeModalWindow();
-};
+function scrollingBtns(event) {
+  let index = originalItems.indexOf(refs.modalImage.src);
 
-const escapeBtnHandler = (event) => {
-  if (event.keyCode !== 27) return;
-  closeModalWindow();
-};
-
-const scrollingBtnHandler = (event) => {
-  let index = originItemsArr.indexOf(refs.image.src);
-
-  if (event.keyCode === 39) {
-    if (index < originItemsArr.length - 1) {
-      refs.image.setAttribute("src", originItemsArr[index + 1]);
+  if (event.code === "ArrowRight") {
+    if (index < originalItems.length - 1) {
+      refs.modalImage.setAttribute("src", originalItems[index + 1]);
     } else {
-      index--;
-      refs.image.setAttribute("src", originItemsArr[index + 1]);
+      index -= 1;
+      refs.modalImage.setAttribute("src", originalItems[index + 1]);
     }
   }
-  if (event.keyCode === 37) {
+
+  if (event.code === "ArrowLeft") {
     if (index === 0) {
-      index = originItemsArr.length;
-      refs.image.setAttribute("src", originItemsArr[index - 1]);
-    } else refs.image.setAttribute("src", originItemsArr[index - 1]);
+      index = originalItems.length;
+      refs.modalImage.setAttribute("src", originalItems[index - 1]);
+    } else {
+      refs.modalImage.setAttribute("src", originalItems[index - 1]);
+    }
   }
+}
+
+const closeModalOverlay = (event) => {
+  if (event.target !== event.currentTarget) return;
+  closeModalBtn();
 };
 
-const openImgItem = (event) => {
+const closeByEsc = (event) => {
+  if (event.code !== "Escape") return;
+  closeModalBtn();
+};
+
+const openImgModal = (event) => {
   event.preventDefault();
-  const imgTarget = event.target;
-  if (imgTarget === event.currentTarget) return;
+  if (event.target === event.currentTarget) return;
 
-  refs.modalWindow.classList.add("is-open");
-  refs.image.alt = imgTarget.alt;
-  refs.image.src = imgTarget.dataset.source;
+  refs.modalOpen.classList.add("is-open");
+  refs.modalImage.alt = event.target.alt;
+  refs.modalImage.src = event.target.dataset.source;
 
-  window.addEventListener("keyup", escapeBtnHandler);
-  window.addEventListener("keyup", scrollingBtnHandler);
+  window.addEventListener("keyup", closeByEsc);
+  window.addEventListener("keydown", scrollingBtns);
 };
 
-const closeModalWindow = () => {
-  refs.modalWindow.classList.remove("is-open");
-  refs.image.alt = "";
-  refs.image.src = "";
-
-  window.removeEventListener("keyup", escapeBtnHandler);
-  window.removeEventListener("keyup", scrollingBtnHandler);
+const closeModalBtn = () => {
+  refs.modalOpen.classList.remove("is-open");
+  refs.modalImage.alt = "";
+  refs.modalImage.src = "";
+  window.removeEventListener("keyup", closeByEsc);
+  window.removeEventListener("keydown", scrollingBtns);
 };
 
-refs.gallery.addEventListener("click", openImgItem);
-refs.btnCloseLightbox.addEventListener("click", closeModalWindow);
-refs.content.addEventListener("click", closeOverlayContent);
+refs.gallery.addEventListener("click", openImgModal);
+refs.modalCloseBtn.addEventListener("click", closeModalBtn);
+refs.modalContent.addEventListener("click", closeModalOverlay);
